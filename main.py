b9f9300d4219c43d4f3c18779fdb3d31060720ce
Josakko <112590914+Josakko@users.noreply.github.com>
2023-03-17 18:32:50 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 import subprocess as sub
-import argparse
 import re
 import time
 
 
 time.sleep(1)
-def get_args():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-i', dest = 'interface')
-  parser.add_argument('-m', dest = 'new_mac')
-  options = parser.parse_args()
-
-  
-  if not options.interface:
-
-    parser.error('[-] Please specify an interface name in the arguments.')
-    
-  elif not options.new_mac:
-
-      parser.error('[-] Please specify new MAC Address.')
-      
-  return options
+interface = ("[+] Enter interface name: ")
+new_mac = ("[+] Enter new MAC: ")
 
 def change_mac(interface, new_mac):
 
@@ -41,17 +26,15 @@ def get_current_mac(interface):
   else:
     print('[-] Could not read the MAC Address')
     
-command_args = get_args()
-
-prev_mac = get_current_mac(command_args.interface)
+prev_mac = get_current_mac(interface)
 print('\n[+] MAC Address before changing -> {}'.format(prev_mac))
 
-change_mac(command_args.interface, command_args.new_mac)
+change_mac(interface, new_mac)
 
-changed_mac = get_current_mac(command_args.interface)
+changed_mac = get_current_mac(interface)
 print('\n[+] MAC Address after change -> {}'.format(changed_mac))
 
-if changed_mac == command_args.new_mac:
+if changed_mac == new_mac:
   print('\n[+] MAC Address was successfully changed from {} to {}'.format(prev_mac, changed_mac))
 else:
   print('\n[-] Could not change the MAC Address')
